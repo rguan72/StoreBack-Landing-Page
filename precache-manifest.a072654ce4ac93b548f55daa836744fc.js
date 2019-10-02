05a2d51dcab5559e7ba22f82698a4adb1db96180
Richard Guan <rguan72@gmail.com>
2019-10-02 12:05:21 -0400

    Updates

---
+++ precache-manifest.a072654ce4ac93b548f55daa836744fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b452d12fb96d1bd251c3558476cb2b1",
+    "url": "/StoreBack-Landing-Page/index.html"
+  },
+  {
+    "revision": "646c38e41a8836e537d7",
+    "url": "/StoreBack-Landing-Page/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "a966b3a70b3e4ca5e93e",
+    "url": "/StoreBack-Landing-Page/static/js/2.90266826.chunk.js"
+  },
+  {
+    "revision": "646c38e41a8836e537d7",
+    "url": "/StoreBack-Landing-Page/static/js/main.dd4c5997.chunk.js"
+  },
+  {
+    "revision": "8b9a298e564b723e5bf2",
+    "url": "/StoreBack-Landing-Page/static/js/runtime-main.93e0e141.js"
+  }
+]);
